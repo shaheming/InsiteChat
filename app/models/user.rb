2cb957321c845ac1a8943e5da1242ef39edd9446
shaheming <hemingsha@gmail.com>
2017-08-05 16:33:59 +0800

    User can login with name or email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,36 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable,:authentication_keys => [:login]
+
+  attr_accessor :login
+  validates :username,
+    :presence => true,
+   :uniqueness => {
+    :case_sensitive => false
+  } 
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  # To aviod using email as user name
+    def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    #Note that this is a “shallow” copy as it copies the object's attributes only, not its associations.
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+
 end
